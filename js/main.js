ac10269340a137defbdd6422302f43986bcc5bc8
abdo adel <abdo48439@gmail.com>
2022-06-15 20:06:43 +0200

    edit

--- js/main.js
+++
@@ -1,4 +0,0 @@
-$("#rightside-nav .sub-menu > a").click(function(e) {
-    $("#rightside-nav ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
-    e.stopPropagation()
-});
